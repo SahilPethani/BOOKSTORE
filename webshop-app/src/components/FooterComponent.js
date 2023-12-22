bd98df3029450b62b992b9ad8924dfc867b32339
damcode1990 <damcode1990@gmail.com>
2023-12-22 15:28:15 +0530

    add code

---
+++ webshop-app/src/components/FooterComponent.js
@@ -0,0 +1,43 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+
+function FooterComponent(props) {
+    return (
+        <div
+            className={
+                props.position === "absolute"
+                    ? props.page === "Home"
+                        ? "footer footer-home footer-absolute"
+                        : "footer footer-absolute"
+                    : "footer"
+            }
+        >
+            <ul className="footer-list">
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaFacebookSquare />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaTwitterSquare />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaLinkedin />
+                    </a>
+                </li>
+                <li className="footer-item">
+                    <a href="#" target="_blank" rel="noreferrer">
+                        <FaInstagramSquare />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default FooterComponent;
